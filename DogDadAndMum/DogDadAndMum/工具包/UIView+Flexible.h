fa47e0dab44b1fa4a0b950b6173ea267125240de
杨方军 <81599010@qq.com>
2015-06-29 11:25:13 +0800

    更新注册页面

---
+++ DogDadAndMum/DogDadAndMum/工具包/UIView+Flexible.h
@@ -0,0 +1,17 @@
+//
+//  UIView+Flexible.h
+//  demo
+//
+//  Created by 小宸宸Dad on 15/6/26.
+//  Copyright (c) 2015年 fj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Flexible)
+
+
+- (instancetype)initWithIPhone5Frame:(CGRect)frame;
+
+
+@end
